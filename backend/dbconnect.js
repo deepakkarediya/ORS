57fcb1c1381a0f1fa0518edf050af2fdf5efc4a4
deepak karediya <deepakkarediya@gmail.com>
2023-12-05 10:47:16 +0530

    7 pagination

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const mongoURI="mongodb://localhost:27017/ORS-APP"
 
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI)
+    mongoose.connect(mongoURI).then(()=>{console.log("connected to mongodb")})
 }
 module.exports=connectToMongo;
